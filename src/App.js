5a946118ea51c5db37053b875bcf9a024e3bd223
Veera Sankar <sankarsivam6991@gmail.com>
2024-12-02 17:08:31 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import FirstPage from "./components/first_page";
 import Header from "./components/header/header";
 
 function App() {
   return (
     <div className="App">
            <Header />
+           <FirstPage/>
     </div>
   );
 }
