65fb54e87813559aceecacb8a9060d2dce7a1c81
Veera Sankar <sankarsivam6991@gmail.com>
2024-12-02 17:16:46 +0530

    Header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@
 import Header from "./components/header/header";
 import OurServices from "./components/our services/ourServices";
 import './App.css';  
+import FirstPage from "./components/first_page";
 
 function App() {
   return (
     <div className="App">
+      <Header/>
+      <FirstPage/>
            <OurServices />
     </div>
   );
