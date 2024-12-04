11c141e44a9b7281b67bd3c330eeff7d6e083830
Joseph <joseph@genniepay.com>
2024-12-04 12:52:26 +0530

    Maximize page done

--- src/App.js
+++ src/App.js
@@ -2,14 +2,10 @@ import Header from "./components/header/header";
 import OurServices from "./components/our services/ourServices";
 import "./App.css";
 import FirstPage from "./components/first_page";
-<<<<<<< HEAD
-import Form from "./components/consultation/Form";
-import Consult from "./components/consultation/Consult";
-import TechStack from "./components/TechStack/techstack";
-=======
 import Consult from "./components/consultation/Consult"
 import Industries from "./components/industries/industries";
->>>>>>> 4485e9e73e55c5dfaaf0b996efeb8dc3572e1c3f
+import TechStack from "./components/TechStack/techstack";
+import Maximize from "./components/maximize/maximize";
 
 function App() {
   return (
@@ -17,6 +13,8 @@ function App() {
       <Header/>
       <FirstPage/>
            <OurServices />
+           <TechStack />
+           <Maximize />
     <Consult />
     <Industries />
     </div>
